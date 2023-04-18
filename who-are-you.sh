9d1937493647c0ad69aa8c79f836972e02dee9cf
aelkarou <karoumi14@gmail.com>
2023-04-18 09:47:54 +0200

    third

--- who-are-you.sh
+++ who-are-you.sh
@@ -1,2 +1,2 @@
 #! bin/bash
-curl -s https://zone01normandie.org/assets/superhero/all.json | jq ' .[] | select( .id == 70 )' | jq ' .name'x
+curl -s https://zone01normandie.org/assets/superhero/all.json | jq ' .[] | select( .id == 70 )' | jq ' .name'
