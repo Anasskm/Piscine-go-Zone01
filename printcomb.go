19a5e4f7b36f66bd5f2b8734e270f81b4b4a8909
aelkarou <karoumi14@gmail.com>
2023-04-19 15:50:31 +0200

    printcomb.go

---
+++ printcomb.go
@@ -0,0 +1,22 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func PrintComb() {
+	for i := '0'; i <= '9'; i++ {
+		for j := '0'; j <= '9'; j++ {
+			for k := '0'; k <= '9'; k++ {
+				if i != j && j != k {
+					z01.PrintRune(i)
+					z01.PrintRune(j)
+					z01.PrintRune(k)
+					if i != '7' && j != '8' && k != '9' {
+						z01.PrintRune(',')
+						z01.PrintRune(' ')
+					}
+				}
+			}
+		}
+	}
+	z01.PrintRune('\n')
+}
