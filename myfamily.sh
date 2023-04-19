898df9c3523121a1b1c146affb18fdbcb5e3f28e
aelkarou <karoumi14@gmail.com>
2023-04-19 09:39:45 +0200

    family

--- myfamily.sh
+++ myfamily.sh
@@ -1,1 +1,2 @@
-curl -s https://zone01normandie.org/assets/superhero/all.json | jq ' .[] | select( .id == 1 )' | jq ' .connections.relatives' | sed 's/"//g'
+HERO_ID="1"
+curl -s https://zone01normandie.org/assets/superhero/all.json | jq ' .[] | select( .id == $HERO_ID )' | jq ' .connections.relatives' | sed 's/"//g'
