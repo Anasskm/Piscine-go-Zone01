8ff91cc191350622052db9ce62d74cfb312b48e7
aelkarou <karoumi14@gmail.com>
2023-04-19 10:24:24 +0200

    family

--- myfamily.sh
+++ myfamily.sh
@@ -1,1 +1,1 @@
-curl -s https://zone01normandie.org/assets/superhero/all.json | jq ' .[] | select( .id == 70 ) ' | jq ' .connections.relatives' | sed 's/"//g'
+curl -s https://zone01normandie.org/assets/superhero/all.json | jq ".[] | select (.id == $HERO_ID)" | jq ' .connections.relatives' | sed 's/"//g'
