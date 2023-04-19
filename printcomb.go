91c2dd4264d54d2727e31eb1dff2e1ada07274a9
aelkarou <karoumi14@gmail.com>
2023-04-19 15:52:10 +0200

    printcomb.go

--- printcomb.go
+++ printcomb.go
@@ -6,7 +6,7 @@ func PrintComb() {
 	for i := '0'; i <= '9'; i++ {
 		for j := '0'; j <= '9'; j++ {
 			for k := '0'; k <= '9'; k++ {
-				if i != j && j != k {
+				if i != j || j != k {
 					z01.PrintRune(i)
 					z01.PrintRune(j)
 					z01.PrintRune(k)
