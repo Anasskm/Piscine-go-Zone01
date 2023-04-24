db7e716fe1956695a0b7598b2459b9ddbf45d36d
aelkarou <karoumi14@gmail.com>
2023-04-24 12:37:34 +0200

    square root

---
+++ sqrt.go
@@ -0,0 +1,18 @@
+package piscine
+
+func Sqrt(nb int) int {
+	if nb < 0 {
+		return 0
+	}
+
+	x := 0
+
+	for i := 0; i < nb; i++ {
+		for j := 0; j < nb; j++ {
+			if i*j == nb && i == j {
+				x = i
+			}
+		}
+	}
+	return x
+}
