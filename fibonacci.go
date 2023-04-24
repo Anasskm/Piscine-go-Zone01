f74244d308e9d44803f5e7dcf170da8189f513c9
aelkarou <karoumi14@gmail.com>
2023-04-24 12:12:30 +0200

    fibonacci

---
+++ fibonacci.go
@@ -0,0 +1,18 @@
+package piscine
+
+func Fibonacci(index int) int {
+	if index < 0 {
+		return -1
+	}
+	if index == 0 {
+		return 0
+	}
+	if index == 1 {
+		return 1
+	}
+
+	if index > 0 {
+		return Fibonacci(index-2) + Fibonacci(index-1)
+	}
+	return 0
+}
