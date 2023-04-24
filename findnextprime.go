73eacfd6ccf91b4076680473ec7395e65a0f3ee3
aelkarou <karoumi14@gmail.com>
2023-04-24 15:27:15 +0200

    find

--- findnextprime.go
+++ findnextprime.go
@@ -7,11 +7,11 @@ func FindNextPrime(nb int) int {
 	if nb%2 == 0 {
 		nb = nb + 1
 	}
-
-	if Ispe(nb) {
-		return nb
-	} else {
-		return FindNextPrime(nb + 2)
+	for {
+		if Ispe(nb) {
+			return nb
+		}
+		nb = nb + 2
 	}
 }
 
