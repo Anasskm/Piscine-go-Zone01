c615b123a6f29189fee32980a5bbe732d0f190b1
aelkarou <karoumi14@gmail.com>
2023-04-24 13:53:52 +0200

    prime

--- isprime.go
+++ isprime.go
@@ -2,12 +2,16 @@ package piscine
 
 func IsPrime(nb int) bool {
 	x := true
+	if nb < 2 {
+		return false
+	}
 	for i := 2; i < nb; i++ {
 		if nb%i == 0 && nb != 2 {
 			x = false
 			break
 
 		}
+		x = true
 	}
 	return x
 }
