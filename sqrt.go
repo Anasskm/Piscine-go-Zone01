fb844e3dd0ee8b2645e61a4fe625ed87f5a05177
aelkarou <karoumi14@gmail.com>
2023-04-24 12:49:39 +0200

    square root

--- sqrt.go
+++ sqrt.go
@@ -1,13 +1,13 @@
 package piscine
 
 func Sqrt(nb int) int {
-	if nb < 0 {
+	if nb <= 0 {
 		return 0
 	}
 
 	x := 0
 
-	for i := 0; i < nb; i++ {
+	for i := 1; i < nb; i++ {
 		if i*i == nb {
 			x = i
 			break
