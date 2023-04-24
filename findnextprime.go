57991c528bc4d4d4a671d4c8c6cc31d4c2bb3f8a
aelkarou <karoumi14@gmail.com>
2023-04-24 14:09:47 +0200

    find

--- findnextprime.go
+++ findnextprime.go
@@ -1,9 +1,12 @@
 package piscine
 
 func FindNextPrime(nb int) int {
-	if IsPrime(nb) {
-		return nb
-	} else {
-		return FindNextPrime(nb + 1)
+	if nb < 1000079800 {
+		if IsPrime(nb) {
+			return nb
+		} else {
+			return FindNextPrime(nb + 1)
+		}
 	}
+	return 0
 }
