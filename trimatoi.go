a284258f4fb12cab616950a62e38bea8ac7f2411
aelkarou <karoumi14@gmail.com>
2023-04-25 19:08:13 +0200

    woaaaah

---
+++ trimatoi.go
@@ -0,0 +1,45 @@
+package piscine
+
+func TrimAtoi(s string) int {
+	x := 0
+	si := 1
+	z := 1
+	if Numeric(s) {
+		for _, i := range s {
+
+			if i < '0' || i > '9' {
+				continue
+			}
+			y := 0
+			for k := '1'; k <= i; k++ {
+				y++
+			}
+			x = (x*10 + y) * z
+		}
+		for j := range s {
+
+			if s[j] >= '0' && s[j] <= '9' {
+				break
+			}
+			if s[j] == '-' {
+				si = -1
+			}
+
+		}
+		return x * si
+	} else {
+		return 0
+	}
+}
+
+func Numeric(s string) bool {
+	r := []rune(s)
+	tr := false
+	for i := range r {
+		if r[i] >= 48 && r[i] <= 57 {
+			tr = true
+			break
+		}
+	}
+	return tr
+}
