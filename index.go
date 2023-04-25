7e4325859e136eb5d24a9eb1eaab7f583ee08a92
aelkarou <karoumi14@gmail.com>
2023-04-25 12:06:57 +0200

    index

--- index.go
+++ index.go
@@ -1,12 +1,17 @@
 package piscine
 
 func Index(s string, toFind string) int {
-	rs := []rune(s)
-	rt := []rune(toFind)
-	for i := range rs {
-		if rs[i] == rt[0] {
-			return i
+	if len(s) > 0 && len(toFind) > 0 {
+		rs := []rune(s)
+
+		rt := []rune(toFind)
+		for i := range rs {
+			if rs[i] == rt[0] {
+				return i
+			}
 		}
+		return -1
+	} else {
+		return -1
 	}
-	return -1
 }
