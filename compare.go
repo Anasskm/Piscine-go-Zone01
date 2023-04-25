37ae41793762fe49143c37e71781bd0103b40af2
aelkarou <karoumi14@gmail.com>
2023-04-25 11:34:44 +0200

    compare

--- compare.go
+++ compare.go
@@ -1,26 +1,11 @@
 package piscine
 
 func Compare(a, b string) int {
-	ra := []rune(a)
-	rb := []rune(b)
-	if len(ra) == len(rb) {
-		count := 0
-		for i := range ra {
-			if ra[i] == rb[i] {
-				count++
-			}
-		}
-		if count != len(ra) {
-			return 1
-		}
+	if a == b {
 		return 0
-	}
-
-	if ra[0] != rb[0] {
+	} else if a > b {
+		return 1
+	} else {
 		return -1
 	}
-	if ra[len(ra)-1] != rb[len(rb)-1] {
-		return 1
-	}
-	return 0
 }
