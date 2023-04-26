259088a3276b4ea39e3ef5518193feeaf284ac34
aelkarou <karoumi14@gmail.com>
2023-04-26 15:24:15 +0200

    pp

--- printprogramname/main.go
+++ printprogramname/main.go
@@ -7,9 +7,18 @@ import (
 )
 
 func main() {
-	r := []rune(os.Args[0])
-	for i := range r {
-		z01.PrintRune(r[i])
+	r := os.Args[0]
+	var k int
+
+	for i := 0; i < len(r); i++ {
+		if r[len(r)-1-i] == '/' {
+			k = len(r) - 1 - i
+			break
+		}
+	}
+	s := r[k+1:]
+	for _, i := range s {
+		z01.PrintRune(i)
 	}
 	z01.PrintRune('\n')
 }
