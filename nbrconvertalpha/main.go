89e5d37e06319bb43e7e7b5ae2785152911dd46a
aelkarou <karoumi14@gmail.com>
2023-04-28 01:41:54 +0200

    pleaaaaaaaaase

--- nbrconvertalpha/main.go
+++ nbrconvertalpha/main.go
@@ -8,45 +8,28 @@ import (
 
 func main() {
 	a := os.Args[1:]
-	z := "abcdefghijklmnopqrstuvwxyz"
 	flag := false
-	for _, i := range a {
-		if i == "--upper" {
+	for i := range a {
+		if i == 0 && a[i] == "--upper" {
 			a = a[1:]
 			flag = true
+			break
 		}
 	}
-
 	for _, i := range a {
-		index := Atoi(i)
-		if index <= 26 && index >= 1 {
+		digit := 0
+		for _, b := range i {
+			digit = digit*10 + int(b-'0')
+		}
+		if digit <= 26 && digit >= 1 {
 			if flag {
-				z01.PrintRune(rune(z[index-1] - 32))
+				z01.PrintRune('a' + rune(digit-32) - 1)
 			} else {
-				z01.PrintRune(rune(z[index-1]))
+				z01.PrintRune('a' + rune(digit) - 1)
 			}
 		} else {
 			z01.PrintRune(' ')
 		}
-
 	}
-
 	z01.PrintRune('\n')
-}
-
-func Atoi(s string) int {
-	x := 0
-
-	for _, i := range s {
-
-		if i < '0' || i > '9' {
-			return 0
-		}
-		y := 0
-		for k := '1'; k <= i; k++ {
-			y++
-		}
-		x = x*10 + y
-	}
-	return x
 }
