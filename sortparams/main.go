82fc02e2cdda6cc1a6f42113b96c5ec6985ff109
aelkarou <karoumi14@gmail.com>
2023-04-27 13:24:07 +0200

    qsfqs

--- sortparams/main.go
+++ sortparams/main.go
@@ -2,37 +2,45 @@ package main
 
 import (
 	"os"
+	"sort"
 
 	"github.com/01-edu/z01"
 )
 
 func main() {
-	a := os.Args
-	for i := 1; i < len(a); i++ {
-		for j := range a[i] {
-			r := []rune(a[i])
-			if r[j] >= '0' && r[j] <= '9' {
+	a := os.Args[1:]
+	sort.Strings(a)
+	for i := 0; i < len(a); i++ {
+		r := []rune(a[i])
+
+		if r[0] >= '0' && r[0] <= '9' {
+			for j := range a[i] {
 				z01.PrintRune(r[j])
-				z01.PrintRune('\n')
 			}
+			z01.PrintRune('\n')
 		}
+
 	}
-	for i := 1; i < len(a); i++ {
-		for j := range a[i] {
-			r := []rune(a[i])
-			if r[j] >= 'A' && r[j] <= 'Z' {
+	for i := 0; i < len(a); i++ {
+		r := []rune(a[i])
+
+		if r[0] >= 'A' && r[0] <= 'Z' {
+			for j := range a[i] {
 				z01.PrintRune(r[j])
-				z01.PrintRune('\n')
 			}
+			z01.PrintRune('\n')
 		}
+
 	}
-	for i := 1; i < len(a); i++ {
-		for j := range a[i] {
-			r := []rune(a[i])
-			if r[j] >= 'a' && r[j] <= 'z' {
+	for i := 0; i < len(a); i++ {
+		r := []rune(a[i])
+
+		if r[0] >= 'a' && r[0] <= 'z' {
+			for j := range a[i] {
 				z01.PrintRune(r[j])
-				z01.PrintRune('\n')
 			}
+			z01.PrintRune('\n')
 		}
+
 	}
 }
