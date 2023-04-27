157ec73eca154773d218768b99c6cf25dd2b16f6
aelkarou <karoumi14@gmail.com>
2023-04-27 19:27:08 +0200

    split

--- splitwhitespaces.go
+++ splitwhitespaces.go
@@ -1,25 +1,19 @@
 package piscine
 
 func SplitWhiteSpaces(s string) []string {
-	k := 1
-
 	word := ""
-	for i := 0; i < len(s)-1; i++ {
-		if s[i] == ' ' && s[i+1] != ' ' {
-			k++
-		}
-	}
-	result := make([]string, k)
-	index := 0
+
+	var result []string
+
 	for _, y := range s {
 		if y != ' ' {
 			word += string(y)
 		} else {
-			result[index] = word
+			result = append(result, word)
 			word = ""
-			index++
+
 		}
 	}
-	result[index] = word
+	result = append(result, word)
 	return result
 }
