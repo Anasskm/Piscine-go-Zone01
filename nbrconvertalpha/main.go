d8360a5df1f8069c523245478483a4ffebaedd03
aelkarou <karoumi14@gmail.com>
2023-04-27 22:50:00 +0200

    nbr

--- nbrconvertalpha/main.go
+++ nbrconvertalpha/main.go
@@ -3,8 +3,6 @@ package main
 import (
 	"os"
 
-	"piscine"
-
 	"github.com/01-edu/z01"
 )
 
@@ -14,7 +12,7 @@ func main() {
 
 	if a[0] == "--upper" {
 		for i := 1; i < len(a); i++ {
-			index := piscine.Atoi(a[i])
+			index := Atoi(a[i])
 			if index > 26 || index <= 0 {
 				z01.PrintRune(' ')
 			} else {
@@ -25,7 +23,7 @@ func main() {
 		}
 	} else {
 		for i := 0; i < len(a); i++ {
-			index := piscine.Atoi(a[i])
+			index := Atoi(a[i])
 			if index > 26 || index <= 0 {
 				z01.PrintRune(' ')
 			} else {
@@ -36,4 +34,27 @@ func main() {
 		}
 	}
 	z01.PrintRune('\n')
+}
+
+func Atoi(s string) int {
+	x := 0
+	si := 1
+	for j, i := range s {
+		if s[j] == '-' && j == 0 {
+			si = -1
+			continue
+		}
+		if s[j] == '+' && j == 0 {
+			continue
+		}
+		if i < '0' || i > '9' {
+			return 0
+		}
+		y := 0
+		for k := '1'; k <= i; k++ {
+			y++
+		}
+		x = x*10 + y
+	}
+	return x * si
 }
