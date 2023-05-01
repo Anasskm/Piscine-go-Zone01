d89c9a81cd3581aca624d24d6aea66c34d0f8c8c
aelkarou <karoumi14@gmail.com>
2023-05-01 19:48:39 +0200

    point

--- point/main.go
+++ point/main.go
@@ -2,36 +2,45 @@ package main
 
 import "github.com/01-edu/z01"
 
-func setPoint(p *[2]int) {
-	p[0] = 42
-	p[1] = 21
+type point struct {
+	x int
+	y int
+}
+
+func setPoint(ptr *point) {
+	ptr.x = 42
+	ptr.y = 21
 }
 
 func main() {
-	var point [2]int
-	points := &point
+	points := &point{}
 
 	setPoint(points)
+	x := "x = "
+	y := " y = "
 
-	p0 := string(point[0])
-	p1 := string(point[1])
-	r1 := []rune(p0)
-	r2 := []rune(p1)
-	z01.PrintRune('x')
-	z01.PrintRune(' ')
-	z01.PrintRune('=')
-	z01.PrintRune(' ')
-	for _, i := range r1 {
-		z01.PrintRune(i)
+	rx := []rune{}
+	ry := []rune{}
+	for points.x != 0 {
+		rx = append(rx, rune(points.x%10)+'0')
+		points.x /= 10
+	}
+	for points.y != 0 {
+		ry = append(ry, rune(points.y%10)+'0')
+		points.y /= 10
+	}
+
+	for _, i := range x {
+		z01.PrintRune(rune(i))
+	}
+	for i := len(rx) - 1; i >= 0; i-- {
+		z01.PrintRune(rx[i])
 	}
 	z01.PrintRune(',')
-	z01.PrintRune(' ')
-	z01.PrintRune('y')
-	z01.PrintRune(' ')
-	z01.PrintRune('=')
-	z01.PrintRune(' ')
-	for _, i := range r2 {
-		z01.PrintRune(i)
+	for _, i := range y {
+		z01.PrintRune(rune(i))
 	}
-	z01.PrintRune('\n')
+	for i := len(ry) - 1; i >= 0; i-- {
+		z01.PrintRune(ry[i])
+	}
 }
