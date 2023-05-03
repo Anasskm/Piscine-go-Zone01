e10442703a74c36515fb0441167d547747a186b5
aelkarou <karoumi14@gmail.com>
2023-05-03 14:33:29 +0200

    sorted

--- issorted.go
+++ issorted.go
@@ -5,7 +5,7 @@ func IsSorted(f func(a, b int) int, a []int) bool {
 	for i := range a {
 		for j := i + 1; j < len(a); j++ {
 			if f(a[i], a[j]) > 0 {
-				for x := j; x < len(a); x++ {
+				for x := i + 1; x < len(a); x++ {
 					for y := j + 1; y < len(a); y++ {
 						if f(a[x], a[y]) < 0 {
 							z = false
@@ -18,7 +18,7 @@ func IsSorted(f func(a, b int) int, a []int) bool {
 
 			}
 			if f(a[i], a[j]) < 0 {
-				for x := j; x < len(a); x++ {
+				for x := i + 1; x < len(a); x++ {
 					for y := j + 1; y < len(a); y++ {
 						if f(a[x], a[y]) > 0 {
 							z = false
