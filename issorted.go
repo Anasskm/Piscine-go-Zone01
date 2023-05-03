e60ad37bb906516e89b8e0d962958dd23789c8d9
aelkarou <karoumi14@gmail.com>
2023-05-03 14:38:27 +0200

    sorted

--- issorted.go
+++ issorted.go
@@ -2,35 +2,30 @@ package piscine
 
 func IsSorted(f func(a, b int) int, a []int) bool {
 	var z bool
-	for i := range a {
-		for j := i + 1; j < len(a); j++ {
-			if f(a[i], a[j]) > 0 {
-				for x := i + 1; x < len(a); x++ {
-					for y := j + 1; y < len(a); y++ {
-						if f(a[x], a[y]) < 0 {
-							z = false
-							break
-						}
-					}
+	if f(a[0], a[1]) < 0 {
+		for i := 1; i < len(a); i++ {
+			for j := i + 1; j < len(a); j++ {
+				if f(a[i], a[j]) > 0 {
+					z = false
+					break
 				}
-				z = true
-				break
-
-			}
-			if f(a[i], a[j]) < 0 {
-				for x := i + 1; x < len(a); x++ {
-					for y := j + 1; y < len(a); y++ {
-						if f(a[x], a[y]) > 0 {
-							z = false
-							break
-						}
-					}
-				}
-				z = true
-				break
-
 			}
 		}
+
+		z = true
 	}
+	if f(a[0], a[1]) > 0 {
+		for i := 1; i < len(a); i++ {
+			for j := i + 1; j < len(a); j++ {
+				if f(a[i], a[j]) < 0 {
+					z = false
+					break
+				}
+			}
+		}
+
+		z = true
+	}
+
 	return z
 }
