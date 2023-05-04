a491807c8ee007d6e7889ae8466cff0335b85e0d
aelkarou <karoumi14@gmail.com>
2023-05-04 17:38:06 +0200

    qsfgq

--- enigma.go
+++ enigma.go
@@ -1,12 +1,5 @@
 package piscine
 
 func Enigma(a ***int, b *int, c *******int, d ****int) {
-	a1 := ***a
-	b1 := *b
-	c1 := *******c
-	d1 := ****d
-	***a = b1
-	*******c = a1
-	****d = c1
-	*b = d1
+	***a, *b, *******c, ****d = *b, ****d, ***a, *******c
 }
