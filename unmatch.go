b49940ccd691f78921af2803f49d33e013d4808d
aelkarou <karoumi14@gmail.com>
2023-05-05 05:15:28 +0200

    qsf

--- unmatch.go
+++ unmatch.go
@@ -2,17 +2,17 @@ package piscine
 
 func Unmatch(a []int) int {
 	var r int
-	b := false
 	for i := 0; i < len(a); i++ {
+		r = 0
 		for j := 0; j < len(a); j++ {
 			if a[i] == a[j] {
-				r = a[i]
-				b = true
+				r++
 			}
 		}
+		if r%2 != 0 {
+			return a[i]
+		}
 	}
-	if !b {
-		return -1
-	}
-	return r
+
+	return -1
 }
