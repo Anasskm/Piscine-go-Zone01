cefa4deee14be63406cc728832a37092e33634f4
aelkarou <karoumi14@gmail.com>
2023-05-05 03:12:03 +0200

    qsf

---
+++ max.go
@@ -0,0 +1,12 @@
+package piscine
+
+func Max(s []int) int {
+	for i := 0; i < len(s)-1; i++ {
+		for j := i + 1; j < len(s); j++ {
+			if s[i] < s[j] {
+				s[i], s[j] = s[j], s[i]
+			}
+		}
+	}
+	return s[len(s)-1]
+}
