2bccec7c5e71755970382f0105460cf4d270f491
aelkarou <karoumi14@gmail.com>
2023-05-09 23:53:57 +0200

    for each if

---
+++ listforeachif.go
@@ -0,0 +1,29 @@
+package piscine
+
+func IsPositiveNode(node *NodeL) bool {
+	switch node.Data.(type) {
+	case int, float32, float64, byte:
+		return node.Data.(int) > 0
+	default:
+		return false
+	}
+}
+
+func IsAlNode(node *NodeL) bool {
+	switch node.Data.(type) {
+	case string:
+		return true
+	default:
+		return false
+	}
+}
+
+func ListForEachIf(l *List, f func(*NodeL), cond func(*NodeL) bool) {
+	originalhead := l.Head
+	for i := l.Head; i != nil; i = i.Next {
+		if cond(i) {
+			f(i)
+		}
+	}
+	l.Head = originalhead
+}
