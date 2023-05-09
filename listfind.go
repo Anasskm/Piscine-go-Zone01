4a71525393d20afa341cb34a08a0cf3f774ddf7b
aelkarou <karoumi14@gmail.com>
2023-05-10 00:31:57 +0200

    find

--- listfind.go
+++ listfind.go
@@ -5,12 +5,10 @@ func CompStr(a, b interface{}) bool {
 }
 
 func ListFind(l *List, ref interface{}, comp func(a, b interface{}) bool) *interface{} {
-	originalhead := l.Head
 	for i := l.Head; i != nil; i = i.Next {
 		if comp(i.Data, ref) {
 			return &i.Data
 		}
 	}
-	l.Head = originalhead
 	return nil
 }
