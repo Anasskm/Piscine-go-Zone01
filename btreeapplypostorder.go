3767d1fdccafe261be3a2973b180a3819fec96a1
aelkarou <karoumi14@gmail.com>
2023-05-11 11:42:15 +0200

    post order

---
+++ btreeapplypostorder.go
@@ -0,0 +1,10 @@
+package piscine
+
+func BTreeApplyPostorder(root *TreeNode, f func(...interface{}) (int, error)) {
+	if root == nil {
+		return
+	}
+	BTreeApplyInorder(root.Left, f)
+	BTreeApplyInorder(root.Right, f)
+	f(root.Data)
+}
