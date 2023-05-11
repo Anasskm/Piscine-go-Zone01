9bb97f197e25ebefdbd8c11a257bd9eee434da02
aelkarou <karoumi14@gmail.com>
2023-05-11 12:23:13 +0200

    post order

--- btreeapplypostorder.go
+++ btreeapplypostorder.go
@@ -1,9 +1,31 @@
 package piscine
 
 func BTreeApplyPostorder(root *TreeNode, f func(...interface{}) (int, error)) {
-	if root != nil {
-		BTreeApplyInorder(root.Left, f)
-		BTreeApplyInorder(root.Right, f)
-		f(root.Data)
+	if root == nil {
+		return
+	}
+
+	stack := []*TreeNode{root}
+	var lastVisited *TreeNode
+
+	for len(stack) > 0 {
+		current := stack[len(stack)-1]
+
+		if lastVisited == nil || lastVisited.Left == current || lastVisited.Right == current {
+			if current.Left != nil {
+				stack = append(stack, current.Left)
+			} else if current.Right != nil {
+				stack = append(stack, current.Right)
+			}
+		} else if current.Left == lastVisited {
+			if current.Right != nil {
+				stack = append(stack, current.Right)
+			}
+		} else {
+			f(current.Data)
+			stack = stack[:len(stack)-1]
+		}
+
+		lastVisited = current
 	}
 }
