0ae787c5cc2e452a874909e5cf86e0cf7025ac86
aelkarou <karoumi14@gmail.com>
2023-05-13 08:50:38 +0200

    transplant

--- btreetransplant.go
+++ btreetransplant.go
@@ -1,24 +1,16 @@
 package piscine
 
-
-
 func BTreeTransplant(root, node, rplc *TreeNode) *TreeNode {
-	
 	if node == root {
 		root = rplc
-	} else if node == node.Parent.Left { 
-		node.Parent.Left = rplc 
-	} else { 
-		node.Parent.Right = rplc /
+	} else if node == node.Parent.Left {
+		node.Parent.Left = rplc
+	} else {
+		node.Parent.Right = rplc
 	}
-
-	
 	if rplc != nil {
 		rplc.Parent = node.Parent
 	}
 
 	return root
-}	
-
-
-
+}
